ac05deff2db91feeab861369319330c41b4fce8e
Dan Korkelia <dan@ramdankorkelia.com>
2020-11-17 10:41:52 +0000

    Initial Commit

---
+++ RonSwansonQuotes/RonSwansonQuotesApp.swift
@@ -0,0 +1,17 @@
+//
+//  RonSwansonQuotesApp.swift
+//  RonSwansonQuotes
+//
+//  Created by Dan Korkelia on 17/11/2020.
+//
+
+import SwiftUI
+
+@main
+struct RonSwansonQuotesApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
